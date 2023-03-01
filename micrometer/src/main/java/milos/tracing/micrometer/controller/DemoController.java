2cccca7ffa0413f70c43d3f29d0f0e5db6b35998
xMilos <m-i-l-o-s@hotmail.com>
2023-03-01 05:04:36 +0100

    Test traceId and spanId

--- micrometer/src/main/java/milos/tracing/micrometer/controller/DemoController.java
+++ micrometer/src/main/java/milos/tracing/micrometer/controller/DemoController.java
@@ -15,6 +15,7 @@ class DemoController {
      * curl -H "x-b3-traceId: 66275a023f6eccfc" -H "x-b3-spanId: 4f5e6d7c8b9a0f1e" http://localhost:8080/hello
      * Response should be {traceId=66275a023f6eccfc, spanId=4f5e6d7c8b9a0f1e}
      * Instead is {traceId=66275a023f6eccfc, spanId=e08b8098bff7a7bd}
+     * Log:  INFO [demo,66275a023f6eccfc,e08b8098bff7a7bd] 11152 --- [nio-8080-exec-1] m.t.m.controller.DemoController          : MDC INFO {traceId=66275a023f6eccfc, spanId=e08b8098bff7a7bd}
      */
     @GetMapping("/hello")
     ResponseEntity<String> hello() {
